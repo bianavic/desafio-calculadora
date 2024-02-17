611cd5a41cab71d5f0e6a8bd303c246a0c02bafe
bianavic <bianavic@gmail.com>
2024-02-17 00:49:24 -0300

    feat: create functions that perform 4 arithmetic operations

---
+++ calculator.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func add(i ...int) int {
+	total := 0
+	for _, v := range i {
+		total += v
+	}
+	return total
+}
+
+func subtract(i ...int) int {
+	total := 0
+	for _, v := range i {
+		total -= v
+	}
+	return total
+}
+
+func multiply(i ...int) int {
+	total := 1
+	for _, v := range i {
+		total *= v
+	}
+	return total
+}
+
+func divide(i ...int) int {
+	total := 1
+	for _, v := range i {
+		total *= v
+	}
+	return total
+}
+
+func main() {
+	sum := add(1, 2, 3)
+	sub := subtract(5, 10)
+	mult := multiply(10, 10)
+	div := divide(20)
+	fmt.Println(sum, sub, mult, div)
+}
